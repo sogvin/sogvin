f6af38d5638e95561dcbdca89107f361d72ed999
Gregory Vincic <g@7de.se>
2019-12-30 21:24:58 +0100

    Pagify dictionary

--- article.go
+++ article.go
@@ -29,31 +29,27 @@ var Index = Html(en,
 
 var Dictionary = Html(en,
 	Head(utf8, viewport, theme, a4),
-	Body(`<header>
-      <span class="left"></span>
-      <code>References</code>
-    </header>
+	Body(
+		header("", "Dictionary"),
+		Article(
+			H1("Dictionary"),
 
-    <article>
-      <h1>Dictionary</h1>
-
-      <p>Short list of words/terms often used in software engineering
+			P(`Short list of words/terms often used in software engineering
 	and sometimes defined differently. Only domain agnostic terms
 	have been listen, for the rest consult an english dictionary.
-	I often use the <code>dict</code> command line tool.</p>
+	I often use the <code>dict</code> command line tool.`),
 
-      <dl>
-	<dt>Argument</dt>
-	<dd>String following the command on the command line.</dd>
+			Dl(
+				Dt("Argument"),
+				Dd("String following the command on the command line."),
 
-	<dt>Flag</dt>
-	<dd>Boolean option.</dd>
+				Dt("Flag"),
+				Dd("Boolean option."),
 
-	<dt>Option</dt>
-	<dd>Argument starting with single or double dashes.</dd>
-
-      </dl>
-    </article>`,
+				Dt("Option"),
+				Dd("Argument starting with single or double dashes."),
+			),
+		),
 		footer,
 	),
 )
