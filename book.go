b14f6d5182a2758d9d2972a133326c0888c08bd7
Gregory Vincic <g@7de.se>
2020-01-06 22:08:07 +0100

    Refactor

--- book.go
+++ book.go
@@ -11,7 +11,6 @@ import (
 
 func NewBook() *Book {
 	book := &Book{}
-
 	toc := Article(
 		H1("Software Engineering"),
 		P("Notes by ", myname),
@@ -21,7 +20,6 @@ func NewBook() *Book {
 			book.AddPage("Design", PurposeOfFuncMain),
 			book.AddPage("Design", NexusPattern),
 			book.AddPage("Design", GracefulServerShutdown),
-
 			gregoryv("draw", "software engineering diagrams"),
 			gregoryv("web", "html generation"),
 		),
@@ -46,10 +44,6 @@ func NewBook() *Book {
 	return book
 }
 
-func linkToPage(page *Page) *Element {
-	return Li(A(Href(page.Filename), findH1(page.Element)))
-}
-
 type Book struct {
 	pages []*Page
 }
@@ -60,6 +54,112 @@ func (book *Book) SaveTo(base string) error {
 		page.SaveTo(base)
 	}
 	return nil
+}
+
+func findH1(article *Element) string {
+	var buf bytes.Buffer
+	w := NewHtmlWriter(&buf)
+	w.WriteHtml(article)
+	from := bytes.Index(buf.Bytes(), []byte("<h1>")) + 4
+	to := bytes.Index(buf.Bytes(), []byte("</h1>"))
+	return strings.TrimSpace(string(buf.Bytes()[from:to]))
+}
+
+func (book *Book) AddPage(right string, article *Element) *Element {
+	filename := filenameFrom(findH1(article)) + ".html"
+	page := newPage(
+		filename,
+		header(right+" - "+A(Href("index.html"), "Software Engineering").String()),
+		article,
+		footer,
+	)
+	book.pages = append(book.pages, page)
+	return linkToPage(page)
+}
+
+func linkToPage(page *Page) *Element {
+	return Li(A(Href(page.Filename), findH1(page.Element)))
+}
+
+func newPage(filename string, header, article, footer *Element) *Page {
+	return NewPage(filename,
+		Html(en,
+			Head(utf8, viewport, theme, a4),
+			Body(header, article, footer),
+		),
+	)
+}
+
+func filenameFrom(in string) string {
+	tidy := bytes.NewBufferString("")
+	var inside bool
+	for _, c := range in {
+		switch c {
+		case '(', ')':
+			continue
+		case ' ':
+			tidy.WriteRune('_')
+		case '<':
+			inside = true
+		case '>':
+			inside = false
+		default:
+			if inside {
+				continue
+			}
+			tidy.WriteString(strings.ToLower(string(c)))
+		}
+	}
+	return tidy.String()
+}
+
+var (
+	en       = Lang("en")
+	utf8     = Meta(Charset("utf-8"))
+	viewport = Meta(
+		Name("viewport"),
+		Content("width=device-width, initial-scale=1.0"),
+	)
+	theme  = stylesheet("theme.css")
+	a4     = stylesheet("a4.css")
+	footer = Footer(myname)
+	myname = "Gregory Vin&ccaron;i&cacute;"
+)
+
+func header(right string) *Element {
+	h := Header()
+	if right != "" {
+		h = h.With(Code(right))
+	}
+	return h
+}
+
+func stylesheet(href string) *Element {
+	return Link(Rel("stylesheet"), Type("text/css"), Href(href))
+}
+
+func boxnote(txt string, cm float64) *Element {
+	return Div(Class("boxnote"),
+		&Attribute{
+			Name: "style",
+			Val:  fmt.Sprintf("margin-top: %vcm", cm),
+		},
+		txt,
+	)
+}
+
+func loadGoFile(filename string, from, to int) string {
+	return `<pre class="srcfile"><code class="go">` +
+		internal.LoadFile(filename, from, to) + "</code></pre>"
+}
+
+func gregoryv(name, txt string) *Element {
+	return Li(
+		fmt.Sprintf(
+			`<a href="https://github.com/gregoryv/%s">%s</a> - %s`,
+			name, name, txt,
+		),
+	)
 }
 
 var GracefulServerShutdown = Article(
@@ -211,105 +311,3 @@ var PurposeOfFuncMain = Article(
 	   work.<br>This works great for services and simpler commands
 	   that only do one thing.`),
 )
-
-func findH1(article *Element) string {
-	var buf bytes.Buffer
-	w := NewHtmlWriter(&buf)
-	w.WriteHtml(article)
-	from := bytes.Index(buf.Bytes(), []byte("<h1>")) + 4
-	to := bytes.Index(buf.Bytes(), []byte("</h1>"))
-	return strings.TrimSpace(string(buf.Bytes()[from:to]))
-}
-
-func (book *Book) AddPage(right string, article *Element) *Element {
-	filename := filenameFrom(findH1(article)) + ".html"
-	page := newPage(
-		filename,
-		header(right+" - "+A(Href("index.html"), "Software Engineering").String()),
-		article,
-		footer,
-	)
-	book.pages = append(book.pages, page)
-	return linkToPage(page)
-}
-
-func filenameFrom(in string) string {
-	tidy := bytes.NewBufferString("")
-	var inside bool
-	for _, c := range in {
-		switch c {
-		case '(', ')':
-			continue
-		case ' ':
-			tidy.WriteRune('_')
-		case '<':
-			inside = true
-		case '>':
-			inside = false
-		default:
-			if inside {
-				continue
-			}
-			tidy.WriteString(strings.ToLower(string(c)))
-		}
-	}
-	return tidy.String()
-}
-
-func newPage(filename string, header, article, footer *Element) *Page {
-	return NewPage(filename,
-		Html(en,
-			Head(utf8, viewport, theme, a4),
-			Body(header, article, footer),
-		),
-	)
-}
-
-var (
-	en       = Lang("en")
-	utf8     = Meta(Charset("utf-8"))
-	viewport = Meta(
-		Name("viewport"),
-		Content("width=device-width, initial-scale=1.0"),
-	)
-	theme  = stylesheet("theme.css")
-	a4     = stylesheet("a4.css")
-	footer = Footer(myname)
-	myname = "Gregory Vin&ccaron;i&cacute;"
-)
-
-func header(right string) *Element {
-	h := Header()
-	if right != "" {
-		h = h.With(Code(right))
-	}
-	return h
-}
-
-func stylesheet(href string) *Element {
-	return Link(Rel("stylesheet"), Type("text/css"), Href(href))
-}
-
-func boxnote(txt string, cm float64) *Element {
-	return Div(Class("boxnote"),
-		&Attribute{
-			Name: "style",
-			Val:  fmt.Sprintf("margin-top: %vcm", cm),
-		},
-		txt,
-	)
-}
-
-func loadGoFile(filename string, from, to int) string {
-	return `<pre class="srcfile"><code class="go">` +
-		internal.LoadFile(filename, from, to) + "</code></pre>"
-}
-
-func gregoryv(name, txt string) *Element {
-	return Li(
-		fmt.Sprintf(
-			`<a href="https://github.com/gregoryv/%s">%s</a> - %s`,
-			name, name, txt,
-		),
-	)
-}
