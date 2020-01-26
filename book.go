4ddcbd1e9b14aae1dacf8d8bfd9cff40a1aef5bd
Gregory Vincic <g@7de.se>
2020-01-26 13:58:08 +0100

    Add components diagram

--- book.go
+++ book.go
@@ -5,7 +5,9 @@ import (
 	"fmt"
 	"strings"
 
-	"github.com/gregoryv/notes/internal"
+	"github.com/gregoryv/draw/shape"
+	"github.com/gregoryv/draw/shape/design"
+	"github.com/gregoryv/sogvin/internal"
 	. "github.com/gregoryv/web"
 )
 
@@ -20,6 +22,7 @@ func NewBook() *Book {
 			book.AddPage("Design", PurposeOfFuncMain),
 			book.AddPage("Design", NexusPattern),
 			book.AddPage("Design", GracefulServerShutdown),
+			book.AddPage("Design", ComponentsDiagram),
 			gregoryv("draw", "software engineering diagrams"),
 			gregoryv("web", "html generation"),
 		),
@@ -160,6 +163,64 @@ func gregoryv(name, txt string) *Element {
 			name, name, txt,
 		),
 	)
+}
+
+var ComponentsDiagram = Article(
+	H1("Components diagram"),
+	P(
+
+		`The components diagram shows services and processes related
+		to one another. Good for system overviews and microservice
+		architectures. Highlight important components.`,
+	),
+	Span(Class("Center"),
+		newOverviewDiagram(),
+	),
+	P(
+		`Use lines between components unless you are conveying
+		signaling direction between them.`,
+	),
+)
+
+func newOverviewDiagram() *Element {
+	shape.ClassAttributes["external"] = `stroke="#d3d3d3" fill="#e2e2e2"`
+	var (
+		d        = design.NewClassDiagram()
+		serviced = shape.NewComponent("serviced")
+		ng       = shape.NewComponent("nginx")
+		inet     = shape.NewCircle(40)
+		client   = shape.NewComponent("client")
+		db       = shape.NewRect("postgres")
+		cloud    = shape.NewLabel("internet")
+	)
+	ng.SetClass("external")
+	client.SetClass("external")
+	inet.SetClass("external")
+	db.SetClass("external")
+
+	d.Place(serviced).At(20, 20)
+	d.Place(db).RightOf(serviced)
+	d.Place(ng).Below(serviced, 70)
+	d.VAlignCenter(serviced, ng)
+	d.Place(inet).RightOf(ng)
+	d.Place(cloud).RightOf(inet)
+	d.VAlignCenter(inet, cloud)
+
+	d.HAlignCenter(ng, inet)
+	d.Place(client).Below(inet, 40)
+	d.VAlignCenter(inet, client)
+
+	lineBetween := func(a, b shape.Shape) {
+		d.Link(a, b).Head = nil
+	}
+	lineBetween(serviced, db)
+	lineBetween(ng, serviced)
+	lineBetween(ng, inet)
+	lineBetween(inet, client)
+
+	src := "img/overview.svg"
+	d.SaveAs("htdocs/" + src)
+	return Img(Src(src))
 }
 
 var GracefulServerShutdown = Article(
