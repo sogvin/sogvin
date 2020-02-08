0cc691fed7a7ba9c0f6e7500108f26f98a7cacac
Gregory Vincic <g@7de.se>
2020-02-08 14:44:33 +0100

    Group packages under separate heading

--- book.go
+++ book.go
@@ -11,24 +11,30 @@ import (
 
 func NewBook() *Book {
 	book := &Book{}
-	toc := Article(
+	toc := Article(Class("toc"),
 		H1("Software Engineering"),
 		P("Notes by ", myname),
 
 		H2("Design"),
-		Ul(Class("toc"),
+		Ul(
 			book.AddPage("Design", PurposeOfFuncMain),
 			book.AddPage("Design", NexusPattern),
 			book.AddPage("Design", GracefulServerShutdown),
 			book.AddPage("Design", ComponentsDiagram),
+		),
+		H3("Go packages"),
+		Ul(
 			gregoryv("draw", "software engineering diagrams"),
 			gregoryv("web", "html generation"),
 		),
 
 		H2("Test"),
-		Ul(Class("toc"),
+		Ul(
 			book.AddPage("Test", InlineTestHelpers),
 			book.AddPage("Test", AlternateDesign),
+		),
+		H3("Go packages"),
+		Ul(
 			gregoryv("golden", "simplify use of golden files"),
 			gregoryv("qual", "quality constraints"),
 			gregoryv("ex", "indented JSON or redirect handler response to stdout"),
@@ -36,7 +42,8 @@ func NewBook() *Book {
 		),
 
 		H2("Build"),
-		Ul(Class("toc"),
+		H3("Go packages"),
+		Ul(
 			gregoryv("stamp", "build information code generator"),
 			gregoryv("find", "files by name or content"),
 		),
