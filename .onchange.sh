b336f7a8f3a956da20fa197639e2aa42d360356b
Gregory Vincic <g@7de.se>
2020-04-25 08:43:01 +0200

    Reload surf onchange

---
+++ .onchange.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -e
+path=$1
+dir=$(dirname "$path")
+filename=$(basename "$path")
+extension="${filename##*.}"
+nameonly="${filename%.*}"
+
+case $extension in
+    go)
+        goimports -w $path
+	go test -coverprofile /tmp/c.out ./...
+	uncover /tmp/c.out
+        ;;
+esac
+reloadsurf.sh
