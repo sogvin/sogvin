0f414006df0c70b38691fd4949240a2809d5b374
Gregory Vincic <g@7de.se>
2020-10-02 09:10:04 +0200

    Refactor

--- example/visitor/visitor_test.go
+++ example/visitor/visitor_test.go
@@ -8,17 +8,17 @@ import (
 
 func Test_visitor_pattern(t *testing.T) {
 	var (
-		Square    = &Square{side: 2}
-		Circle    = &Circle{radius: 3}
-		Rectangle = &Rectangle{width: 3, height: 2}
+		square    = &Square{side: 2}
+		circle    = &Circle{radius: 3}
+		rectangle = &Rectangle{width: 3, height: 2}
 		equals    = asserter.Wrap(t).Equals
 	)
 
-	equals(CalcArea(Square), 4.0)
-	equals(CalcArea(Circle), 18.84955592153876)
-	equals(CalcArea(Rectangle), 6.0)
+	equals(CalcArea(square), 4.0)
+	equals(CalcArea(circle), 18.84955592153876)
+	equals(CalcArea(rectangle), 6.0)
 
-	equals(FindMiddle(Square), "1,1")
-	equals(FindMiddle(Circle), "3,3")
-	equals(FindMiddle(Rectangle), "1,1")
+	equals(FindMiddle(square), "1,1")
+	equals(FindMiddle(circle), "3,3")
+	equals(FindMiddle(rectangle), "1,1")
 }
