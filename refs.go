e5d7632aa5805a426d31f6d236cfa4c03b66bdb9
Gregory Vincic <g@7de.se>
2021-04-04 13:58:47 +0200

    Fix links

--- refs.go
+++ refs.go
@@ -1,14 +1,12 @@
 package sogvin
 
 import (
-	"path"
-
 	. "github.com/gregoryv/web"
 )
 
 func github(pth, label string) *Element {
 	return A(
-		Href(path.Join("https://github.com", pth)),
+		Href("https://github.com/"+pth),
 		label,
 	)
 }
