56039f8be22d9272c59fae92972b6907206b091c
Gregory Vincic <g@7de.se>
2021-04-11 22:23:00 +0200

    Use /tmp/sogvin during build

--- ci.sh
+++ ci.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
-case $1 in
-    publish)
-	# cleanup
-	find . -name "*~" -delete
+dist=/tmp/sogvin
 
-	# deploy
-	rsync -avC ./docs/ www.7de.se:/var/www/www.sogvin.com/
+case $1 in
+    -h)
+	echo "Usage: $0 build|publish"
+	;;
+    publish)
+	rsync -avC $dist/docs/ www.7de.se:/var/www/www.sogvin.com/
+	;;
+    build)
+	mkdir -p $dist
+	go run ./cmd/mksite -p $dist/docs
+	rsync -avC ./docs $dist/
+	;;
+    clean)
+	rm -rf $dist
 	;;
     *)
-	echo "Usage: $0 publish"
+	$0 build
 	;;
 esac
--- cmd/mksite/main.go
+++ cmd/mksite/main.go
@@ -1,15 +1,19 @@
 package main
 
 import (
+	"flag"
 	"log"
 
 	"github.com/gregoryv/sogvin"
 )
 
 func main() {
+	prefix := flag.String("p", "./docs", "write pages to")
+	flag.Parse()
+
 	book := sogvin.NewSoftwareEngineeringBook()
 	log.SetFlags(0)
-	err := book.SaveTo("./docs")
+	err := book.SaveTo(*prefix)
 	if err != nil {
 		log.Fatal(err)
 	}
