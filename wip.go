e1b39d4fad16ef97a5c6ea5b12140a8c2441419e
Gregory Vincic <g@7de.se>
2021-04-20 07:06:28 +0200

    Add wip file with notes

---
+++ wip.go
@@ -0,0 +1,21 @@
+package sogvin
+
+/*
+Naming packages
+
+place of doing or concept
+
+- language, english
+- protocol, http
+- net, library, kitchen
+
+
+Organizing packages by prefix like tools in a workshop
+
+- wrench 1,2,3,4 ....
+- philips edge 2 3 4
+
+what if we could find a similar system, depending on what you want to
+do, but variations of that with a prefix?
+
+*/
