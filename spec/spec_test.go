71e52db795892a8499cb6bebdb6ea85d4886bf88
Gregory Vincic <g@7de.se>
2021-10-06 07:26:39 +0200

    Fix firefox rendering issue

--- spec/spec_test.go
+++ spec/spec_test.go
@@ -15,7 +15,13 @@ func Test_index(t *testing.T) {
 	NewPage(
 		Html(
 			Head(
+				Meta(Charset("utf-8")),
 				Style(theme()),
+				Script(
+					// to prevent Firefox FOUC, this must be here
+					// https://stackoverflow.com/questions/21147149
+					"let FF_FOUC_FIX;",
+				),
 			),
 			body,
 		),
