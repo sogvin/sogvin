9f7d14b406093b834ef73910279e45d1f06e2c5f
Gregory Vincic <g@7de.se>
2021-10-06 07:01:53 +0200

    Reformat spec

--- spec/spec.go
+++ spec/spec.go
@@ -54,38 +54,33 @@ func NewBeachStory(n *Hn) *Element {
 	return Article(
 		n.H1("To the beach"),
 
-		Section(
-			n.H2("Background"),
+		P(`Through the navigation system people can plot a course or
+		manually steer a ship.  People depend on its accuracy and
+		automation to safely navigate through space.`),
 
-			P(`Through the navigation system people can plot a course
-            or manually steer a ship.  People depend on its accuracy
-            and automation to safely navigate through space.`),
+		n.H2(`Plot new course`),
 
-			n.H3(`Plot new course`),
+		P(`Standing at the bridge, the captain asks for the closest
+		viable planets for some time at the beach. Selects the one
+		with the nicest beaches and tells the system to plot the
+		course. The plot details show that the route is through
+		uncharted space. The captain selects another of the viable
+		planets and tells the system to plot the course again. Once
+		satisfied, he tells the system to engage.`),
 
-			P(`Standing at the bridge, the captain asks for the
-			closest viable planets for some time at the beach. Selects
-			the one with the nicest beaches and tells the system to
-			plot the course. The plot details show that the route is
-			through uncharted space. The captain selects another of
-			the viable planets and tells the system to plot the course
-			again. Once satisfied, he tells the system to engage.`),
+		P(`The journey is estimated to five days. On the second day
+		however an interference is detected in space and the ship
+		adapts the course accordingly. The captain is notified through
+		his personal communicator of the changes.`),
 
-			P(`The journey is estimated to five days. On the second
-			day however an interference is detected in space and the
-			ship adapts the course accordingly. The captain is
-			notified through his personal communicator of the
-			changes.`),
+		n.H2(`Manual control`),
 
-			n.H3(`Manual control`),
-
-			P(`Once the ship enters the planets atmosphere one of the
-	 	    crew members on the bridge tells the system to let him
-	 	    manually steer the ship. He wants to find a suitable spot
-	 	    on the crowded beach, before letting the passengers leave
-	 	    the ship.`),
-			//
-		),
+		P(`Once the ship enters the planets atmosphere one of the crew
+	 	members on the bridge tells the system to let him manually
+	 	steer the ship. He wants to find a suitable spot on the
+	 	crowded beach, before letting the passengers leave the
+	 	ship.`),
+		//
 	)
 }
 
