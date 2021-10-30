32956ac462e42e12450b65866ef48c841795b903
Gregory Vincic <g@7de.se>
2021-10-30 15:54:14 +0200

    Add some empty lines

--- internal/cmd/countstars/main.go
+++ internal/cmd/countstars/main.go
@@ -6,17 +6,21 @@ import (
 )
 
 func main() {
+
 	galaxy := "milkyway"
 	flag.StringVar(
 		&galaxy, "galaxy",
 		galaxy, "name of galaxy to count stars in",
 	)
+
 	verbose := false
 	flag.BoolVar(
 		&verbose, "verbose",
 		verbose, "enables verbose logging",
 	)
-	flag.Parse()
+
+	flag.Parse() // exits on error
+
 	SetVerbose(verbose)
 	CountStars(os.Stdout, galaxy)
 }
