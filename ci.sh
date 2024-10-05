c6dfdaa091afba670d1b4d167bbcdc4d9a4eb2da
Gregory Vincic <gregoryvse@gmail.com>
2024-10-05 10:34:05 +0200

    Add generate target in ci.sh

--- ci.sh
+++ ci.sh
@@ -6,33 +6,42 @@ set -o pipefail
 dist=/tmp/website
 
 case $1 in
-    -h)
-	echo "Usage: $0 build|publish"
-	;;
-    publish)
-	go run ./cmd/mksite -c # guard
-	rsync -avC $dist/docs/ www.7de.se:/var/www/www.website.com/
-	;;
-    build)
+    g|generate)
+	echo "generate"	
 	pushd ../
 	tree -P "*.go" -I "*_test.go" navstar | \
 	    grep -v directories > website/example/navstar.tree
 	popd
+	;;
+    b|build)
+	echo "build"	
 	go build ./...
 	
 	mkdir -p $dist
 	go run ./cmd/mksite -p $dist/docs
+	# update static files
 	rsync -aC ./docs $dist/
+	echo "dist: $dist"
 	;;
-    clean)
-	rm -rf $dist
-	;;
-    test)
+    t|test)
+	echo "test"	
 	go test -coverprofile /tmp/c.out ./... 2>&1 | \
 	    sed -e 's| of statements||g' \
 		-e 's|coverage: ||g' \
 		-e 's|github.com/gregoryv/website|.|g' | \
 	    grep -v "no test"
+	;;
+    publish)
+	echo "publish"
+	go run ./cmd/mksite -c # guard
+	rsync -avC $dist/docs/ www.7de.se:/var/www/www.website.com/
+	;;
+    clean)
+	echo "clean"	
+	rm -rf $dist
+	;;
+    -h)
+	echo "Usage: $0 build|publish"
 	;;
     *)
 	$0 build test
